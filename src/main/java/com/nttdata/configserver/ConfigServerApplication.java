04f05ebbad3b6a3ed6eae8d569179489a03a79d4
Alberto Jesus Romero Herencia <aromeroh@emeal.nttdata.com>
2022-04-08 19:41:50 -0500

    Code challenge implemenatacion config server con spring cloud

---
+++ src/main/java/com/nttdata/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.nttdata.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Config-Server application, enable config server.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
+}
